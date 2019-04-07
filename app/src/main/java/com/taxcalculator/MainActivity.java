d47ad6b3bf64b99eb00bde23bd4518f8dc51b363
ManishShubhu <lifestartedfrom1997@gmail.com>
2019-04-07 21:45:31 +0545

    Tax Calculator Complete

--- app/src/main/java/com/taxcalculator/MainActivity.java
+++ app/src/main/java/com/taxcalculator/MainActivity.java
@@ -2,12 +2,52 @@ package com.taxcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnCalculate;
+    private EditText etSalary;
+    private TextView tvOutput;
+
+    double salary, res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCalculate = findViewById(R.id.btnCalculate);
+        etSalary= findViewById(R.id.etSalary);
+        tvOutput = findViewById(R.id.tvOutput);
+
+        btnCalculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnCalculate) {
+            if (validation()) {
+                salary = Double.parseDouble(etSalary.getText().toString())*12;
+                TaxCalc obj = new TaxCalc();
+                obj.setSalary(salary);
+                res = obj.calculateTax();
+                tvOutput.setText("Your Tax is " + Double.toString(res));
+            }
+        }
+    }
+
+    public boolean validation(){
+        boolean flag = true;
+        if (TextUtils.isEmpty(etSalary.getText().toString())) {
+            etSalary.setError("Please enter Salary");
+            etSalary.requestFocus();
+            flag = false;
+        }
+        return flag;
     }
 }
